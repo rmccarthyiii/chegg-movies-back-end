24226c03021984f367534521aaa296dbe5d4e19a
Robert McCarthy <romccarthy@indeed.com>
2024-02-28 04:03:41 -0600

    update server.js file

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-const { PORT = 5001 } = process.env; // macbook pro m1 might need a different port, eg: 3002
+const { PORT = 10000 } = process.env; // macbook pro m1 might need a different port, eg: 3002
 
 const app = require("./app");
 const knex = require("./db/connection");
